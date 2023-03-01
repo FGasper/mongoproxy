daaa9889c9b35b235366310bfc0689ffe02be9e9
Felipe Gasper <felipe@felipegasper.com>
2023-03-01 09:54:13 -0500

    it runs … kind of

--- server/config/installed_modules.go
+++ server/config/installed_modules.go
@@ -2,4 +2,4 @@ package config
 
 import _ "github.com/mongodbinc-interns/mongoproxy/modules/bi"
 import _ "github.com/mongodbinc-interns/mongoproxy/modules/mockule"
-import _ "github.com/mongodbinc-interns/mongoproxy/modules/mongod"
+//import _ "github.com/mongodbinc-interns/mongoproxy/modules/mongod"
